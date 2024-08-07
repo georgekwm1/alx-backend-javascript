43a4402164439c5d5385d3228858dc30b1b163bd
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-07 15:39:21 +0100

    updataed file

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,4 +1,3 @@
-const getSumOfHoods = (initialNumber, expansion1989, expansion2019) => initialNumber
-    + (expansion1989 === undefined ? 89 : expansion1989) + (expansion2019 === undefined ? 19
-  : expansion2019);
-export default getSumOfHoods;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
