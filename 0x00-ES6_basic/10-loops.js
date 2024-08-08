a4144169f91495f8131ef89aa79f0a0c2c5ebe4a
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-08 14:59:06 +0100

    update

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => appendString + value);
+  let idx = 0;
+  for (const value of array) {
+    array[idx] = appendString + value;
+    idx += 1;
+  }
+
+  return array;
 }
