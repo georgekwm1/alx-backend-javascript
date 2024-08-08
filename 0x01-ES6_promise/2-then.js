7a44f50c9c5dbb918d6a3f1a4d032587962903c6
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-09 00:17:34 +0100

    modified 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'Success' };
-  })
-    .catch(() => {});
+  return promise.then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => {})
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
