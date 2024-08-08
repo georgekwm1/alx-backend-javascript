9650f7e4256ed11b882cab0e65bae49beb8781bd
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-08 20:53:15 +0100

    created 0-promise.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+import createEmployeesObject from './11-createEmployeesObject.js';
+
+
+export default function createReportObject(employeesList) {
+
+    for (let occupation in employeesList){
+
+}
+}
+
+employeesList = createEmployeesObject()
