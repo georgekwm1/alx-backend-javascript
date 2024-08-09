0247ec4643a118c07b23c9af8e4a13e3d543aa35
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-09 14:00:43 +0100

    updated 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return signUpUser(firstName, lastName)
     .then((user) => uploadPhoto(fileName, user.userId))
-    .then((user) => user);
+    .then((user) => user)
+    .catch(() => Error());
 }
