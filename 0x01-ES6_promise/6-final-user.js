ac3ded6fe6ec9635cc66f7d6a2e761b97a666bbc
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-09 14:45:17 +0100

    updated 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return signUpUser(firstName, lastName)
-    .then((user) => uploadPhoto(fileName, user.userId))
-    .then((user) => user)
-    .catch(() => Error());
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
