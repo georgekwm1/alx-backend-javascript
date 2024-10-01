905e250f87876b5df54952e347b673dc48f29a95
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-10-01 23:07:55 +0100

    "Replaced readline module with process.stdin and process.stdout for user input and output"

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,26 @@
 
-const readline = require('readline')
+// const readline = require('readline')
 
-const question = 'Welcome to Holberton School, what is your name?\n'
-const response = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+// const question = 'Welcome to Holberton School, what is your name?\n'
+// const response = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// response.question(question, (answer) => {
+//     console.log(`Your name is: ${answer}`)
+//     response.close()
+// })
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-response.question(question, (answer) => {
-    console.log(`Your name is: ${answer}`)
-    response.close()
-})
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
